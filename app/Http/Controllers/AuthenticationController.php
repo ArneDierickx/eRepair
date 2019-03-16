4dd2372a924ecd0889ffad91fd8c7fb2bd066b35
Dierickx Arne <arne.dierickx@student.howest.be>
2019-03-16 13:31:40 +0100

    prototype laravel

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class AuthenticationController extends Controller
+{
+    public function login()
+    {
+        $username = request("username");
+        $password = request("password");
+        $token = auth("api")->attempt(["username" => $username, "password" => $password]);
+        if (!$token) {
+            return response()->json(["error" => "Unauthorized"], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    private function respondWithToken($token)
+    {
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => "bearer",
+            "expires_in" => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function register()
+    {
+        $user = User::create([
+            "username" => request("username"),
+            "password" => request("password")
+        ]);
+
+        $token = auth("api")->login($user);
+
+        return $this->respondWithToken($token);
+    }
+
+    public function logout()
+    {
+        auth("api")->logout();
+    }
+}
